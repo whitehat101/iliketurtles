63ebe8a066d24b61b0f029370d3609fc3351242c
Jeremy Ebler <jebler@gmail.com>
2014-11-14 21:30:48 -0800

    refactor pserver - wip

--- programs/pserver.lua
+++ programs/pserver.lua
@@ -1,5 +1,30 @@
 -- Peripheral Server
 
+--[[
+
+Request all methods from a server
+{
+  [1] = 'poll',
+  filter = filter -- optional
+}
+
+Call a single method with optional args
+{
+  [1] = 'call',
+  filter = filter, -- optional
+  method = 'method',
+  args = { ... } -- optional
+}
+
+Call several methods without arguments
+{
+  [1] = 'scan',
+  filter = filter, -- optional
+  methods = {'method1', 'method2', 'method3'},
+}
+
+]]--
+
 -- Collect Args
 local argv = {...}
 assert(#argv >= 2, 'Usage: '..shell.getRunningProgram()..' protocol hostname')
@@ -27,7 +52,7 @@ local function scanPeripherals()
       print('Found: '..name..' '..kind)
       table.insert(peripherals, setmetatable({
         name = name,
-        kind = kind,
+        ['type'] = kind,
         methods = peripheral.getMethods(name),
       },{
         __index = peripheral.wrap(name)
@@ -36,54 +61,132 @@ local function scanPeripherals()
   end
 end
 
+local function copy(obj, seen)
+  if type(obj) ~= 'table' then return obj end
+  if seen and seen[obj] then return seen[obj] end
+  local s = seen or {}
+  local res = setmetatable({}, getmetatable(obj))
+  s[obj] = res
+  for k, v in pairs(obj) do res[copy(k, s)] = copy(v, s) end
+  return res
+end
+
+local Each = {
+  __index = {
+    each = function (self, callback)
+      for i,v in ipairs(self) do
+        callback(i,v)
+      end
+    end
+  }
+}
+
+local function getPeripherals(filter)
+  filter = filter or {}
+  local matches = copy(peripherals)
+
+  -- filter by device name/label
+  if filter.name then
+    for i=#matches, 1, -1 do
+      if labels[device.name] ~= filter.name then
+        table.remove(matches, i)
+      end
+    end
+  end
+
+  -- filter by device type
+  if filter.type then
+    for i=#matches, 1, -1 do
+      if device.type ~= filter.type then
+        table.remove(matches, i)
+      end
+    end
+  end
+
+  return setmetatable(matches, Each)
+end
+
+local app
+
+function app.poll(request)
+  local devices = getPeripherals(request.filter)
+  local response = {}
+
+  devices.each(function(i, device)
+    table.insert(response, {
+      kind = device.kind,
+      response = device.methods,
+      name = labels[device.name],
+    })
+  end)
+
+  return response
+end
+
+function app.call(request)
+  local devices = getPeripherals(request.filter)
+  local args = request.args or {}
+  local response = {}
+  assert(request.method, 'Required param #method missing')
+
+  devices.each(function(i, device)
+    -- prepare method call
+    local call = { device[request.method] }
+    for i,v in ipairs(args) do table.insert(call, v) end
+
+    -- call in protected mode
+    local _, result = pcall(unpack(call))
+
+    table.insert(response, {
+      kind = device.kind,
+      response = result,
+      name = labels[device.name],
+    })
+  end)
+
+  return response
+end
+
+function app.scan(request)
+  local devices = getPeripherals(request.filter)
+  local args = request.args or {}
+  local response = {}
+  assert(request.methods, 'Required param #methods missing')
+
+  devices.each(function(i, device)
+    local results = {}
+    for i, method in ipairs(request.methods) do
+      local _, result = pcall(device[method])
+      results[method] = result
+    end
+
+    table.insert(response, {
+      kind = device.kind,
+      response = results,
+      name = labels[device.name],
+    })
+  end)
+
+  return response
+end
+
 -- local pcall(peripheral.call, name, isWireless)
 
 local function respondTo(request)
-  local responses = {
+  -- Promote action string to action method on app object
+  request[1] = app[request[1]]
+
+  -- Call action
+  local success, body = pcall(request)
+
+  return textutils.serialize({
     -- host = os.getComputerLabel() or ''..os.getComputerID(),
     host = hostname,
     day = os.day(),
     time = os.time(),
-    timestamp = 'Day '..os.day()..', '..textutils.formatTime(os.time())
-  }
-  for i, device in ipairs(peripherals) do
-    if not request.kind or device.kind == request.kind then
-      if not request.name or labels[device.name] == request.name then
-        if type(request.method) == 'string' and request.method ~= '' then
-          if device[request.method] then
-            local call = request.argv or {}
-            table.insert(call, 1, device[request.method])
-            local _, result = pcall(unpack(call))
-            table.insert(responses, {
-              kind = device.kind,
-              response = result,
-              name = labels[device.name],
-            })
-          end
-        elseif type(request.methods) == 'table' then
-          local results = {}
-          for i, method in request.methods do
-            if device[request.method] then
-              local _, result = pcall(device[method])
-              results[method] = result
-            end
-          end
-          table.insert(responses, {
-            kind = device.kind,
-            response = results,
-            name = labels[device.name],
-          })
-        else
-          table.insert(responses, {
-            kind = device.kind,
-            response = device.methods,
-            name = labels[device.name],
-          })
-        end
-      end
-    end
-  end
-  return textutils.serialize(responses)
+    success = success,
+    response = response,
+  })
 end
 
 -- Application Threads
